e6ab673a91e4ec79fa34a8fafe322834e8d19367
dlkm22 <115967278+dlkm22@users.noreply.github.com>
2023-11-15 15:50:05 +0800

    Coded MSSQL and MYSQL queries

--- functions/sqlconn.py
+++ functions/sqlconn.py
@@ -4,6 +4,7 @@ eg
 cursor.execute("SELECT * FROM Throughput WHERE DeviceName = ?", data['DeviceName'])
 '''
 import pyodbc
+import datetime
 '''
 For developer:
 Hints:
@@ -22,14 +23,25 @@ when IsWindowsAuth is True:
                      'Trusted_Connection=yes;'
 '''
 def connect(dbhostname: str, dbport: int, 
-            database: str = "master", 
+            database: str = "", 
             username: str = "", password: str = "", isWindowsAuth: bool=True, 
             rawconnstr: str = ""):
     try:
-        conn = pyodbc.Connection
+        if (isWindowsAuth == True):
+            conn = pyodbc.connect('Driver=ODBC Driver 17 for SQL Server;'
+                      f'Server={dbhostname};'
+                      f'Database={database};'
+                      'Trusted_Connection=yes;')
+        else:
+            conn = f'DRIVER={{ODBC Driver 18 for SQL Server}};
+                    SERVER={dbhostname};
+                    DATABASE={database};
+                    UID={username};
+                    PWD={password}'
     except:
         conn = None
     return conn
+
 '''
 HINT:
 SELECT SQL.
@@ -38,7 +50,20 @@ if filters empty, dont use WHERE.
 '''
 def returnrows(conn : pyodbc.Connection, 
                tablename: str, rows: list = [], filters: list = []):
-    return tuple
+    
+    sql_query = f"SELECT {', '.join(rows) if rows else '*'} FROM {tablename}"
+    #WHERE clause if filter is applied
+    if filters:
+        where_clause = ' AND '.join(filters)
+        sql_query += f' WHERE {where_clause}'
+
+    # Execute the SQL query and fetch the results
+    cursor = conn.cursor()
+    cursor.execute(sql_query)
+    rows = cursor.fetchall()
+
+    return rows
+
 '''
 HINT:
 INSERT SQL.
@@ -49,18 +74,69 @@ try except for those SQL errors. (not right columns etc), return a -1.
 if all goes well, return amt of rows affected
 '''
 def insertrows(conn : pyodbc.Connection, tablename : str, values: tuple, insertrows : list = []):
-    return int
+    try:
+        cursor = conn.cursor()
+
+        # if insertrows value
+        if insertrows:
+            columns = ', '.join(insertrows)
+            placeholders = ', '.join(['?' for _ in range(len(insertrows))])
+            sql_query = f"INSERT INTO {tablename} ({columns}) VALUES ({placeholders})"
+        else:
+            # if insertrows has no value
+            columns = ', '.join([col[0] for col in cursor.columns(table=tablename)])
+            placeholders = ', '.join(['?' for _ in range(len(values[0]))])
+            sql_query = f"INSERT INTO {tablename} ({columns}) VALUES ({placeholders})"
+
+        # insert query for each set of values
+        rowcount = 0
+        for val_set in values:
+            cursor.execute(sql_query, val_set)
+            rowcount += cursor.rowcount
+
+        # Commit the changes
+        conn.commit()
+
+        return rowcount  #int 
+
+    except pyodbc.Error as e:
+        print(f"Error: {e}")
+        return -1 #print error message and return -1
 '''
 HINT:
 UPDATE SQL.
 UPDATE {table} SET {col} = {value} WHERE {condition}
 '''
 def updatesinglerows(conn : pyodbc.Connection, tablename : str, colname : str, value: str, filters : list = []):
-    return int
+
+    # filters
+    where_clause = " AND ".join(filters)
+
+    # Update query
+    sql_query = f"UPDATE {tablename} SET {colname} = {value} WHERE {where_clause}"
+
+    cursor = conn.cursor()
+    cursor.execute(sql_query)
+    conn.commit()
+
+    # Return the no. of rows affected
+    return cursor.rowcount
 '''
 HINT:
 DELETE SQL.
 DELETE FROM {table} WHERE {condition}
 '''
 def deleterows(conn: pyodbc.Connection, tablename: str, filters: list = []):
-    return int
+    # filters, can delete multiple rows based on filters
+    where_clause = " AND ".join(filters)
+
+    # Delete query
+    sql_query = f"DELETE FROM {tablename} WHERE {where_clause}"
+
+    cursor = conn.cursor()
+    cursor.execute(sql_query)
+    conn.commit()
+    
+    
+    # Return the no. of rows affected
+    return cursor.rowcount
